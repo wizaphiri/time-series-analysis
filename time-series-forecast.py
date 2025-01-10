fce13efb978e9658a7e3dd1301f37d28cd40f3f9
Wiza <64467592+wizaphiri@users.noreply.github.com>
2025-01-10 16:14:55 +0200

    Create time-series-forecast.py
    Basic Time series forecasting demo with ARIMA and ADF

---
+++ time-series-forecast.py
@@ -0,0 +1,20 @@
+# basic time series analysis with ARIMA, starting with Augmented Dickey-Fuller (ADF) test
+
+# Exploratory Data Analysis (EDA)
+    # Visualize the Data: Plot the time series to identify patterns, trends, and seasonality.
+    # Stationarity Check (Non-stationary data must be differenced to remove trends or seasonality)
+
+
+# use ARIMA
+
+# Model Estimation
+    # Fit the ARIMA model using the identified parameters (𝑝,𝑑,𝑞) and the historical data with statsmodels lib
+
+# Forecasting
+    # Generate future values based on the fitted model for the desired forecast horizon.
+    # Provide confidence intervals to quantify uncertainty in the predictions
+
+# Model Evaluation
+    # Compare forecasted values with actual values (if available) using metrics such as: MAE, MAPE, RMSE
+
+# Model deployment
